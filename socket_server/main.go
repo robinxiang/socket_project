3716a19382af217a0d5f66e153341234aa695b2d
robinxiang <dancelinux@gmail.com>
2023-11-13 17:12:21 +0800

    update

--- socket_server/main.go
+++ socket_server/main.go
@@ -10,6 +10,7 @@ import (
 )
 
 func handleConnectionFunc(conn net.Conn, serverConf common.Server_config) {
+	defer conn.Close() //when exit close the net.conn
 	var (
 		this_server common.Server_config
 		// server_start_msg string
@@ -35,7 +36,6 @@ func handleConnectionFunc(conn net.Conn, serverConf common.Server_config) {
 		}
 	}()
 
-	defer conn.Close() //when exit close the net.conn
 	//read data to the socket buffer
 	read_msg_length, err := conn.Read(read_buffer)
 	if err != nil {
@@ -60,23 +60,25 @@ func main() {
 		ServerPort:     1122,
 		ServerProtocol: "tcp",
 	}
+	server_start_msg = fmt.Sprintln("SCRET Socket Server is creating...")
+	//make serverip:port format string
+	str_ip_port = fmt.Sprintf("%s:%d", this_server.ServerIp, this_server.ServerPort)
+
+	//create listenner
+	listener, err := net.Listen(this_server.ServerProtocol, str_ip_port)
+
+	//if create listener got err
+	if err != nil {
+		fmt.Printf("Can't listen on the %s:%d \n error quit!\n %s", this_server.ServerIp, this_server.ServerPort, err)
+		os.Exit(0) // go err,exit the server program
+	}
+
+	//if listen has running
+	fmt.Println(server_start_msg) // print the server start msg
+	fmt.Printf("Server is listen on (%s)%s : %d\n", this_server.ServerProtocol, this_server.ServerIp, this_server.ServerPort)
+
+	//loop for listen the income connect
 	for {
-		server_start_msg = fmt.Sprintln("SCRET Socket Server is creating...")
-		//make serverip:port format string
-		str_ip_port = fmt.Sprintf("%s:%d", this_server.ServerIp, this_server.ServerPort)
-
-		//create listenner
-		listener, err := net.Listen(this_server.ServerProtocol, str_ip_port)
-
-		//if create listener got err
-		if err != nil {
-			fmt.Printf("Can't listen on the %s:%d \n error quit!\n %s", this_server.ServerIp, this_server.ServerPort, err)
-			os.Exit(0) // go err,exit the server program
-		}
-
-		//if listen has running
-		fmt.Println(server_start_msg) // print the server start msg
-		fmt.Printf("Server is listen on (%s)%s : %d\n", this_server.ServerProtocol, this_server.ServerIp, this_server.ServerPort)
 
 		conn, err = listener.Accept()
 		// if listener got error
@@ -84,9 +86,10 @@ func main() {
 			fmt.Printf("connection go error! %s\n error quit!", err)
 			os.Exit(0)
 		}
-		// defer conn.Close() //when exit close the net.conn
-
 		// start gorouteine to run handleConnectionFunc
 		go handleConnectionFunc(conn, this_server)
 	}
+
+	defer conn.Close() //when exit close the net.conn
+
 }
