6d0f7036528413a128ad4cc66257c3013c4386f4
robin@hg <dancelinux@gmail.com>
2023-11-26 22:23:17 +0000

    update

--- micro_service/example_protobuf/server/main.go
+++ micro_service/example_protobuf/server/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
 
+	"github.com/robinxiang/socket_project/micro_service/example_protobuf/server/invoicer/github.com/robinxiang/socket_project/micro_service/example_protobuf/server/invoicer"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":8089")
+	if err != nil {
+		log.Fatal("create listener error%s", err)
+	}
+	serverRegister := grpc.NewServer
+	invoicer.RegisterInvoicerServer()
 }
