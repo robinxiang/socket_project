c6e9bcfb409b28ed5daee2e05243939cc169c666
robin@hg <dancelinux@gmail.com>
2023-11-26 22:57:28 +0000

    update

--- micro_service/example_protobuf/server/main.go
+++ micro_service/example_protobuf/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
@@ -8,11 +9,29 @@ import (
 	"google.golang.org/grpc"
 )
 
+type myInvoicerServer struct {
+	invoicer.UnimplementedInvoicerServer
+}
+
+func (s *myInvoicerServer) Create(context.Context, *invoicer.CreateRequest) (*invoicer.CreateResponse, error) {
+	return &invoicer.CreateResponse{
+		Pdf:  []byte("test pdf"),
+		Docx: []byte("test Docx"),
+	}, nil
+
+}
+
 func main() {
 	lis, err := net.Listen("tcp", ":8089")
 	if err != nil {
 		log.Fatal("create listener error%s", err)
 	}
-	serverRegister := grpc.NewServer
-	invoicer.RegisterInvoicerServer()
+	serverRegister := grpc.NewServer()
+	service := &myInvoicerServer{}
+	invoicer.RegisterInvoicerServer(serverRegister, service)
+	err = serverRegister.Serve(lis)
+	if err != nil {
+		log.Fatal("create serve error:%s", err)
+
+	}
 }
