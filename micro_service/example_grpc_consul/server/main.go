4054525d704258095739742938e0729dd45832b1
robin@hg <dancelinux@gmail.com>
2023-11-28 15:35:27 +0000

    update

--- micro_service/example_grpc_consul/server/main.go
+++ micro_service/example_grpc_consul/server/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/robinxiang/socket_project/micro_service/example_grpc_consul/pbsocket_project/micro_service/example_grpc_consul/pb"
+	"google.golang.org/grpc"
+)
+
+type Childen struct {
+}
+
+// bind class
+func (c *Childen) SayHello(ctx context.Context, in *pb.Person, opts ...grpc.CallOption) (*pb.Person, error) {
+	in.Name = "Hello " + in.Name
+	return in, nil
+}
+
 func main() {
+	// init grpc object
+	grpcServer := grpc.NewServer()
+
+	// register service
+	pb.RegisterHelloServer(grpcServer, new(Childen))
+
+	// setup listener
+	listenner, err := net.Listen("tcp", "111.47.170.131:8090")
+	if err != nil {
+		fmt.Println("setup listener error:", err)
+	}
+	defer listenner.Close()
+
+	// start server
 
 }
